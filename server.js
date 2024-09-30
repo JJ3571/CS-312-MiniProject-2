808e81b8027c1798a432fb1a27987a7c2cd47ad2
Jarred D. <jsd246@nau.edu>
2024-09-30 14:49:05 -0700

    release v1.0

--- server.js
+++ server.js
@@ -13,9 +13,28 @@ app.use(express.static(__dirname + '/public'));
 
 
 
+// --- Cors Setup --- 
+import cors from 'cors'
+
+const corsOptions = {
+    origin: 'localhost:3000',
+    optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions))
+
+
+
 // --- Body Parser Setup ---
 import bodyparser from "body-parser";
 app.use(bodyparser.urlencoded({ extended: true }));
+
+
+
+// --- Environment Variables ---
+import dotenv from 'dotenv'
+dotenv.config();
+const OMDB_API_KEY = process.env.OMDB_API_KEY;
 
 
 
@@ -24,12 +43,35 @@ app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
 
+// --- Axios & Page Routes ---
+import axios from 'axios'
 
-// --- Page Routes ---
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', { movie: null, error: null }); // Movie needs to be null to avoid an immediate error!
 });
 
+app.get('/movie', (req, res) => { // Redirect route
+    res.redirect('/');
+});
+
+app.post('/movie', async (req, res) => {
+    const title = req.body.title;
+    if (!title) {                 // If no title is entered, redirect to the home page
+        return res.redirect('/');
+    }
+
+    const apiKey = OMDB_API_KEY;
+    const url = `http://www.omdbapi.com/?t=${title}&apikey=${apiKey}`;
+
+    try {
+        const response = await axios.get(url);
+        const movie = response.data;
+        const posterUrl = `http://img.omdbapi.com/?i=${movie.imdbID}&h=600&apikey=${apiKey}`;
+        res.render('index', { movie: movie, poster: posterUrl, error: null });
+    } catch (error) {
+        res.render('index', { movie: null, poster: null, error: 'Error, please try again' });
+    }
+});
 
 
 // --- Server Start ---
